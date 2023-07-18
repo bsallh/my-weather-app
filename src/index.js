05025ae35fbcf3241883f9f678579bc1ac1e64a4
bsallh <bsallh019@gmail.com>
2023-07-18 12:49:26 -0600

    changed apiKey to brackets

--- src/index.js
+++ src/index.js
@@ -44,7 +44,7 @@ function changeTemp(response) {
 
 function searchCity(city) {
   let apiKey = "84fc8tob0a63d91c4609042a3b47d99c";
-  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=84fc8tob0a63d91c4609042a3b47d99c&units=metric`;
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
   axios.get(apiUrl).then(changeTemp);
 }
 console.log(apiUrl);
