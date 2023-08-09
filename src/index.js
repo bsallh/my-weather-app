98e0cec954126e1c4e1b5a6da98a1fd6d9832a95
bsallh <bsallh019@gmail.com>
2023-08-09 14:34:40 -0600

    forecast api integration

--- src/index.js
+++ src/index.js
@@ -22,24 +22,36 @@ let dateTime = document.getElementById("date-time");
 dateTime.innerHTML =
   day + " " + hours + ":" + (minutes < 10 ? "0" : "") + minutes + " " + ampm;
 
+
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
 
   let forecastHTML = `<div class="row">`;
-  let days =["Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
-  days.forEach(function (day) {
+  forecast.forEach(function (forecastDay, index) {
+    if(index < 6) {
     forecastHTML =
       forecastHTML +
       `
       <div class="col-2">
-        <div class="weather-forecast-date">${day}</div>
-        <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/scattered-clouds-day.png" alt="" width="42"/>
+        <div class="weather-forecast-date">${formatDay(forecastDay.dt)}</div>
+        <img src="http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${forecastDay.weather[0].icon}.png" alt="" width="42"/>
         <div class="weather-forecast-temperatures">
-          <span class="weather-forecast-temperature-max">4°</span>
-          <span class="weather-forecast-temperature-min">-4°<span>
+          <span class="weather-forecast-temperature-max">${Math.round(forecastDay.temperature.maximum)}°</span>
+          <span class="weather-forecast-temperature-min">${Math.round(forecastDay.temperature.minimum)}°<span>
         </div>
       </div>
       `;
+    }
   });
 
           forecastHTML = forecastHTML + `</div>`;
